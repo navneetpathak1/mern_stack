909200edf6becbd730869507266449a08eac6dcc
navneetpathak <navneetpathak1909@gmail.com>
2025-02-07 13:26:42 +0530

    orm

--- sql/week-13-sql-master/src/utils.ts
+++ sql/week-13-sql-master/src/utils.ts
@@ -1,7 +1,7 @@
 import { Client } from 'pg'; 
 
 export async function getClient() {
-    const client = new Client("postgresql:admin:-0mCYPvhBZP9s593NuSHSK1extnzh_@us-east-1.24137400-88da-47de-82d2-fec85d1f0990.aws.yugabyte.cloud:5433/ yugabyte?ssl=true&sslmode=verify-full&sslrootcert=Cluster: lustrous-armadillo YSQL Username: admin YSQL Password: -0mCYPvhBZP9s593NuSHSK1extnzh_ YCQL Username: admin YCQL Password: -0mCYPvhBZP9s593NuSHSK1extnzh_");
+    const client = new Client(" postgresql://NAVNEET:NAVNEETPATHAK@localhost:5432/NavneetDB");
     await client.connect();
     return client;
 }
